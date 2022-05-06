ea9657c201c98c7c1abd58e1598cdf4e234b7890
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-05-06 14:28:24 +0200

    Adding tests/Fixtures/Models/User.php

---
+++ tests/Fixtures/Models/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Fixtures\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'email'
+    ];
+}
