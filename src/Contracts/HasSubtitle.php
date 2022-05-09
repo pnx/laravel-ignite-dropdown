812f3d627865fee68e55e7526a4ef2d84b2dfd6f
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-05-09 14:21:33 +0200

    Adding src/Contracts/HasSubtitle.php

---
+++ src/Contracts/HasSubtitle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ignite\Contracts;
+
+interface HasSubtitle
+{
+    /**
+     * Get the objects subtitle.
+     *
+     * @return string
+     */
+    public function getSubtitle() : string;
+}
